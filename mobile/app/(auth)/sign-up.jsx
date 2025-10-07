cec86d33a1d07db63cf2bf1682e347f6c9514640
Saurav Dhoju <sauravdhoju12@gmail.com>
2025-10-07 22:24:14 +0545

    changes in the auto scroll keybaord

--- mobile/app/(auth)/sign-up.jsx
+++ mobile/app/(auth)/sign-up.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
-import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View, KeyboardAvoidingView } from 'react-native';
 import { useSignUp } from '@clerk/clerk-expo';
 import { useRouter } from 'expo-router';
 import { styles } from "@/assets/styles/auth.styles.js";
 import { Ionicons } from '@expo/vector-icons';
 import { COLORS } from "../../constants/colors.js";
 import { Image } from 'expo-image';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 export default function SignUpScreen() {
   const { isLoaded, signUp, setActive } = useSignUp()
@@ -96,7 +97,12 @@ export default function SignUpScreen() {
   }
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <KeyboardAwareScrollView
+      style={{ flex: 1 }}
+      contentContainerStyle={{ flexGrow: 1 }}
+      enableOnAndroid={true}
+      enableAutomaticScroll={true}
+    >
       <View style={styles.container}>
         <Image source={require("../../assets/images/createAccount.png")} style={styles.illustration} />
         <Text style={styles.title}>Create Account</Text>
@@ -140,6 +146,6 @@ export default function SignUpScreen() {
           </TouchableOpacity>
         </View>
       </View>
-    </View>
+    </KeyboardAwareScrollView>
   )
 }
